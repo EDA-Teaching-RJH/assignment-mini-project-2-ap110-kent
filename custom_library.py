e6b6384c269f93f62e60cbba704053e25e716bc2
ap110-kent <ap110@kent.ac.uk>
2026-03-24 13:26:29 +0000

    same as before

--- custom_library.py
+++ custom_library.py
@@ -9,7 +9,7 @@ class GradeCalculator:
     @staticmethod
     def get_letter_grade(percentage=None):
         """Convert percentage to letter grade"""
-        # BUG 26: No parameter validation
+        
         if percentage >= 90:
             return 'A'
         elif percentage >= 80:
@@ -25,11 +25,9 @@ class GradeCalculator:
     def get_grade_distribution(grades):
         """Calculate distribution of letter grades"""
         distribution = {}
-        # BUG 27: Missing default values for letters
         
         for grade in grades:
             letter = GradeCalculator.get_letter_grade(grade)
-            # BUG 28: KeyError if letter not in distribution
             distribution[letter] += 1
         
         return distribution
@@ -37,5 +35,4 @@ class GradeCalculator:
     @staticmethod
     def is_passing(grade, passing_score=60):
         """Check if a grade is passing"""
-        # BUG 29: Returns None instead of boolean for non-numeric
         return grade >= passing_score
